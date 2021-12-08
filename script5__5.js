be9a4e7f97f088ef42dd4e3d404a3d899bef6b7a
Davit <90.davitmanukyan@mail.ru>
2021-12-08 23:13:37 +0400

    receives a string, finds possible largest numbers

---
+++ script5__5.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function myFunc5(string) {
+  let str = "";
+  let num = 0;
+  let arr = string.split("");
+
+  for (let i = 0; i < arr.length + 1; i++) {
+    let x = Number(arr[i]);
+    if (!isNaN(x)) {
+      str += arr[i];
+    } else if (arr[i] === "-") {
+      str += arr[i];
+    } else {
+      str = Number(str);
+      num += str;
+      str = "";
+    }
+  }
+  console.log(num);
+}
+
+myFunc5("FwrtY45KHL120mn10P");
+myFunc5("Wert12lop-12");
